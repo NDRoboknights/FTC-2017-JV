d1829291baf9ea36703d3ba1ac957f927dbaf52f
chiefwithbeef44 <34226571+chiefwithbeef44@users.noreply.github.com>
2018-01-13 21:49:09 -0800

    Added new enums
    Direction Forward and Backward and Status, Beginning, Working, and finished

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/utils/Status.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.utils;
+
+/**
+ * Created by sambl on 1/13/2018.
+ */
+
+public enum Status {
+
+    BEGINNING(1), WORKING(2), FINISHED(3);
+
+    public int v;
+    Status(int v){
+        this.v = v;
+    }
+}
